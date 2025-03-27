c2b31a31d704a15377faf55b2c7532110eae44a2
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2025-03-27 19:49:07 +0500

    add vendor modal is completed

--- src/common/components/AddVendorModal.jsx
+++ src/common/components/AddVendorModal.jsx
@@ -1,12 +1,59 @@
 import { CrossIcon } from "@assets/svgs";
-import { ModalBody, ModalWrapper } from "@common/components";
+import { Button, ModalBody, ModalWrapper } from "@common/components";
 
 export function AddVendorModal({ crossIconClick }) {
   return (
     <ModalWrapper>
       <ModalBody className='relative'>
         <CrossIcon className='absolute top-2 right-2' onClick={crossIconClick} />
-        <h1>This is modal</h1>
+        <h1>Add Vendor</h1>
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor=''>Name</label>
+            <input type='text' name='' id='' placeholder='Enter Name' className='bg-white rounded-md h-12 px-4' />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor='' className='text-black'>
+              Email
+            </label>
+            <input type='email' name='' id='' placeholder='Enter Email' className='bg-white rounded-md h-12 px-4' />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor='' className='text-black'>
+              Company Name
+            </label>
+            <input
+              type='text'
+              name=''
+              id=''
+              placeholder='Enter Company Name'
+              className='bg-white rounded-md h-12 px-4'
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label htmlFor='' className='text-black'>
+              Mobile Number
+            </label>
+            <input
+              type='number'
+              name=''
+              id=''
+              placeholder='Enter Mobile Number'
+              className='bg-white rounded-md h-12 px-4'
+            />
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label for='cars'>Type</label>
+            <select name='cars' id='cars' className='bg-white rounded-md h-12 px-4'>
+              <option value='volvo'>Ticket</option>
+              <option value='saab'>Hotel</option>
+            </select>
+          </div>
+          <div className='flex gap-3 justify-end'>
+            <Button className='bg-blue-600 min-w-[3.75rem]' title='Add' />
+            <Button className='bg-red-600' title='Cancel' />
+          </div>
+        </div>
       </ModalBody>
     </ModalWrapper>
   );
--- src/common/components/ModalBody.jsx
+++ src/common/components/ModalBody.jsx
@@ -1,6 +1,6 @@
 export function ModalBody({ children }) {
   return (
-    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg z-10 w-[600px] h-[70vh] p-6'>
+    <div className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-sky-50 p-5 rounded-lg z-10 w-[600px] h-fit max-h-[80vh] p-6 overflow-y-auto scrollbar-none'>
       {children}
     </div>
   );
