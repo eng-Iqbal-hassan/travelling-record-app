31dc371e61cd621a3e35ce4a88e5f645402d32bf
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2025-05-30 22:59:09 +0500

    tickets data is get

--- src/common/components/TicketTable.jsx
+++ src/common/components/TicketTable.jsx
@@ -1,61 +1,33 @@
-import { Table, TableBody, TableHead, TableRow, Th, TableData } from "@common/components";
+import { Table, TableBody, TableHead, TableRow, Th, TableData, Button } from "@common/components";
 
-export function TicketTable() {
-  const TicketTableData = [
-    {
-      serial: "1",
-      date: "01/01/2024",
-      description: "4",
-      debit: "90.00",
-      credit: "40.00",
-      balance: "180",
-    },
-    {
-      serial: "1",
-      date: "01/01/2024",
-      description: "4",
-      debit: "90.00",
-      credit: "40.00",
-      balance: "180",
-    },
-    {
-      serial: "1",
-      date: "01/01/2024",
-      description: "4",
-      debit: "90.00",
-      credit: "40.00",
-      balance: "180",
-    },
-    {
-      serial: "1",
-      date: "01/01/2024",
-      description: "4",
-      debit: "90.00",
-      credit: "40.00",
-      balance: "180",
-    },
-  ];
+export function TicketTable({ data }) {
   return (
     <Table>
       <TableHead>
         <TableRow>
           <Th text='Sr. No' />
           <Th text='Date' />
+          <Th text='Vendor' />
           <Th text='Description' />
           <Th text='Debit (Sell)' />
           <Th text='Credit (Buy)' />
           <Th text='Balance' />
+          <Th text='Action' />
         </TableRow>
       </TableHead>
       <TableBody>
-        {TicketTableData.map((data, index) => (
-          <TableRow key={index}>
-            <TableData text={data.serial} />
-            <TableData text={data.date} />
-            <TableData text={data.description} />
-            <TableData text={data.debit} />
-            <TableData text={data.credit} />
-            <TableData text={data.balance} />
+        {data.map((item, index) => (
+          <TableRow key={index} className='h-[3.75rem]'>
+            <TableData text={index + 1} />
+            <TableData text={item.date || "-"} />
+            <TableData text={item.vendor.name || "-"} />
+            <TableData text={item.description || "-"} />
+            <TableData text={item.debit || "-"} />
+            <TableData text={item.credit || "-"} />
+            <TableData text={item.balance || "-"} />
+            <div className='p-2.5'>
+              <Button className='bg-blue-600' title='Email' />
+            </div>
           </TableRow>
         ))}
       </TableBody>
