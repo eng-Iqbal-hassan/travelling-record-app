4ad519d3cd32996b628416e019701003ff61b0d1
Iqbal Hassan <iqbal.hassan@cogentlabs.co>
2025-05-31 13:13:52 +0500

    get request for hotel is made

--- src/pages/reservation/components/Reservation.jsx
+++ src/pages/reservation/components/Reservation.jsx
@@ -1,8 +1,19 @@
 import { AddReservationModal, Button, Header, ReservationTable } from "@common/components";
 import { useState } from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
 export function Reservation() {
   const [openReservationModal, setOpenReservationModal] = useState(false);
+  const queryClient = useQueryClient();
+  const hotelsQuery = useQuery({
+    queryKey: ["hotels"],
+    queryFn: async () => {
+      const response = await axios.get("http://54.164.99.34//api/hotels/all/");
+      return response.data.hotels;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
   return (
     <div>
       <Header />
@@ -11,7 +22,7 @@ export function Reservation() {
           <h2>Reservation</h2>
           <Button className='bg-[#000080]' title='Add Reservation' onClick={() => setOpenReservationModal(true)} />
         </div>
-        <ReservationTable />
+        {hotelsQuery.data && <ReservationTable data={hotelsQuery.data} />}
       </div>
       {openReservationModal && (
         <AddReservationModal
